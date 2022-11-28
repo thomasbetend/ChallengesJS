4d7a6ad323a450300e4520a44d8e8b899e8d05e3
thomasbetend <thomas.betend@yahoo.fr>
2022-11-28 13:56:38 +0100

    challenge 03 accepted

--- script.js
+++ script.js
@@ -1,12 +1,45 @@
 'use strict';
 
-let a = 1, b = 1;
+let Bernard = {
+    weight: 65,
+    height: 1.88
+};
 
-let c = ++a; 
-let d = b++; 
+let Marcel = {
+    weight: 85,
+    height: 1.76
+};
 
-// a vaut 2
-// b vaut 2
-// c vaut 2
-// d vaut 1
+function imc (weight, height) {
+    return parseFloat(weight / (height * height)).toFixed(1);
+};
 
+let imcBernard = imc(Bernard.weight, Bernard.height);
+let imcMarcel = imc(Marcel.weight, Marcel.height);
+
+let message = "";
+
+if (imcBernard > imcMarcel) {
+    message = "Bernard a un IMC(" + imcBernard + ") plus élevé que Marcel(" + imcMarcel + ").";
+} else {
+    message = "Marcel a un IMC(" + imcMarcel + ") plus élevé que Bernard(" + imcBernard + ").";
+}
+
+console.log(message);
+
+function imcMessage (person) {
+    if (imc(person.weight, person.height) <18.5) {
+        return "Insuffisance pondérale";
+    } else if (imc(person.weight, person.height) <24.9) {
+        return "Normal";
+    } else if (imc(person.weight, person.height) <29.9) {
+        return "Surpoids";
+    } else if (imc(person.weight, person.height) <40) {
+        return "Obésité";
+    } else {
+        return "Obésité massive";
+    }
+}
+
+console.log(imcMessage(Bernard));
+console.log(imcMessage(Marcel));
