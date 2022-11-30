f9c8f7e28885aaa0d4b22d505e38d658b5051045
thomasbetend <thomas.betend@yahoo.fr>
2022-11-30 09:47:34 +0100

    correction

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function billSentence (bill) {
 
     let tip = 0;
     let total = bill + tip;
-    (50 <= bill <= 300) ? tip = 0.15 * bill : tip = 0.20 * bill;
+    (50 <= bill && bill <= 300) ? tip = 0.15 * bill : tip = 0.20 * bill;
 
     return "La note était de " + bill + ", le pourboire de " + tip + " et la valeur totale de " + total; 
 }
