3f19ec5b96668e8321d3c075e15fd18157319925
thomasbetend <thomas.betend@yahoo.fr>
2022-11-30 12:33:14 +0100

    challenge 11 accepted

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const bernard1 = {
     weight: 78,
     height: 1.69,
     calcBMI: function () {
-        return this.weight / Math.pow(this.height, 2);
+        return parseFloat(this.weight / Math.pow(this.height, 2)).toFixed(2);
     },
 }
 
@@ -14,19 +14,19 @@ const marcel1 = {
     weight: 92,
     height: 1.95,
     calcBMI: function () {
-        return this.weight / Math.pow(this.height, 2);
+        return parseFloat(this.weight / Math.pow(this.height, 2)).toFixed(2);
     },
 }
 
-marcel1.bmi = marcel1.calcBMI();
 bernard1.bmi = bernard1.calcBMI();
+marcel1.bmi = marcel1.calcBMI();
 
 const bernard2 = {
     name: 'Bernard',
     weight: 95,
     height: 1.88,
     calcBMI: function () {
-        return this.weight / Math.pow(this.height, 2);
+        return parseFloat(this.weight / Math.pow(this.height, 2)).toFixed(2);
     },
 }
 
@@ -35,21 +35,21 @@ const marcel2 = {
     weight: 85,
     height: 1.76,
     calcBMI: function () {
-        return this.weight / Math.pow(this.height, 2);
+        return parseFloat(this.weight / Math.pow(this.height, 2)).toFixed(2);
     },
 }
 
-marcel2.bmi = marcel1.calcBMI();
-bernard2.bmi = bernard1.calcBMI();
+marcel2.bmi = marcel2.calcBMI();
+bernard2.bmi = bernard2.calcBMI();
 
 function compareBMI (obj1, obj2) {
-    if (obj1.calcBMI() > obj2.calcBMI()) {
+    if (obj1.bmi > obj2.bmi) {
         console.log(
-            `L’IMC de ${obj1.name} (${parseFloat(obj1.calcBMI().toFixed(2))}) est plus élevé que celui de ${obj2.name} (${parseFloat(obj2.calcBMI().toFixed(2))})`
+            `L’IMC de ${obj1.name} (${obj1.bmi}) est plus élevé que celui de ${obj2.name} (${obj2.bmi})`
         )
     } else {
         console.log(
-            `L’IMC de ${obj2.name} (${parseFloat(obj2.calcBMI().toFixed(2)) }) est plus élevé que celui de ${obj1.name} (${parseFloat(obj1.calcBMI().toFixed(2))})`
+            `L’IMC de ${obj2.name} (${obj2.bmi}) est plus élevé que celui de ${obj1.name} (${obj1.bmi})`
         )
     }
 }
@@ -57,5 +57,4 @@ function compareBMI (obj1, obj2) {
 
 compareBMI(bernard1, marcel1);
 compareBMI(bernard2, marcel2);
-console.log(marcel2.BMI2);
 
