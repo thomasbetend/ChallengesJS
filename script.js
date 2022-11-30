9ac4df5e07b9df3dc62b7a2d5865d6175ce9df01
thomasbetend <thomas.betend@yahoo.fr>
2022-11-30 15:01:08 +0100

    challenge 13 accepted

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 'use strict';
 
+// Exercice 1
+
 const game = {
     team1: 'Bayern Munich',
     team2: 'Borrussia Dortmund',
@@ -32,7 +34,7 @@ const game = {
         ],
     ],
     score: '4:0',
-    scored: ['Lewandowski', 'Gnarby', 'Lewandowski','Hummels'],
+    scored: ['Lewandowski', 'Gnabry', 'Lewandowski','Hummels'],
     date: 'Nov 9th, 2037',
     odds: {
         team1: 1.33,
@@ -97,3 +99,93 @@ printGoals();
 // 7
 
 console.log(`${game.team1} will certainly win, according to odds...`);
+
+
+
+// Exercice 2
+
+// 1 
+
+for (let i =0; i < game.scored.length; i++) {
+    console.log(`But ${i+1} : ${game.scored[i]}`);
+}
+
+// 2
+
+const odds = Object.values(game.odds)
+
+let sumOdds = 0;
+let averageOdds = 0;
+
+for (let i = 0; i < odds.length; i++) {
+    sumOdds += odds[i];
+    averageOdds = sumOdds / (i + 1)
+}
+
+console.log(averageOdds);
+
+// 3 
+
+console.log(`Probabilité de victoire pour le ${game.team1} : ${odds[0]}`);
+console.log(`Probabilité d'un match nul: ${odds[1]}`);
+console.log(`Probabilité de victoire pour le ${game.team2} : ${odds[2]}`);
+
+// 4
+
+const scorers = {
+    Gnabry: 1,
+    Lewandowski: 2
+}
+
+
+// Exercice 3
+
+const gameEvents = [
+    [17, "⚽ GOAL"],
+    [36, "🔁 Substitution"],
+    [47, "⚽ GOAL"],
+    [61, "🔁 Substitution"],
+    [64, "🟨 Yellow card"],
+    [69, "🟥 Red card"],
+    [70, "🔁 Substitution"],
+    [72, "🔁 Substitution"],
+    [76, "⚽ GOAL"],
+    [80, "⚽ GOAL"],
+    [92, "🟨 Yellow card"],
+];
+
+// 1 
+
+let events = [];
+
+for (let i = 0; i < gameEvents.length; i++) {
+    if (!(events.includes(gameEvents[i][1]))) {
+        events.push(gameEvents[i][1]);
+    } 
+}
+
+console.log(events);
+
+// 2
+
+gameEvents.splice(4, 1);
+
+console.log(gameEvents);
+
+// 3
+
+const averageEvent = 90 / gameEvents.length;
+
+console.log(
+    `Un évènement est apparu en moyenne toutes les ${averageEvent} minutes.`
+)
+
+// 4 
+
+for (let i = 0; i < gameEvents.length; i++) {
+    if (gameEvents[i][0] <= 45) {
+        console.log(`[PREMIÈRE MOITIÉ] ${gameEvents[i][0]} . ⚽️ ${gameEvents[i][1]}`)
+    } else {
+        console.log(`[SECONDE MOITIÉ] ${gameEvents[i][0]} . ⚽️ ${gameEvents[i][1]}`)
+    }
+}
